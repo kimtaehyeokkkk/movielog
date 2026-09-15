31ee83be28655b5847f3133e59ad199f28706889
unknown <tateth2009@naver.com>
2026-09-15 21:01:17 +0900

    feat: MovieLog 시작 화면을 구현한다

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:movielog/movie_log_app.dart';
+
+void main() {
+  runApp(const MovieLogApp());
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:movielog/movie_log_app.dart';
+
+void main() {
+  testWidgets('MovieLog start screen shows title text', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MovieLogApp());
+
+    // Verify that the start screen title is shown.
+    expect(find.text('영화의 순간을 기록하세요'), findsOneWidget);
+    expect(find.text('시작하기'), findsOneWidget);
+  });
+}
